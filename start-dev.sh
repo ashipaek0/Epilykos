9f6056eda79eae3c35d264aaa56654088d60cd16
ashipaek0 <folabiklan@gmail.com>
2026-06-28 08:03:24 +0100

    chore: untrack local dev scripts

--- start-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export SETTINGS_PASSWORD=ashipa
-export PORT=3002
-exec /usr/bin/node server.js
--- start-with-pass.js
+++
@@ -1,7 +0,0 @@
-const olog = console.warn;
-const fs = require('fs');
-console.warn = (...args) => {
-  fs.appendFileSync('/tmp/epilykos-pass.log', args.join(' ') + '\n');
-  olog.apply(console, args);
-};
-require('./server.js');
