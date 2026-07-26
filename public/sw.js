ee8f51b66e780c8df276cb0bf0438159ecb3f0a7
ashipaek0 <folabiklan@gmail.com>
2026-07-26 09:51:58 +0100

    chore: bump sw cache v17→v18

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@
  *   navigator.serviceWorker.controller.postMessage({ type: 'network-config', localURL, remoteURL })
  */
 
-const CACHE_NAME = 'epilykos-v17';
+const CACHE_NAME = 'epilykos-v18';
 const STATIC_ASSETS = [
   '/',
   '/style.css',
